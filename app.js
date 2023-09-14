96dcd9c0459d9c2b78a59f9a8c36e6b01bf54cfb
chinochavero <123489844+chinochavero@users.noreply.github.com>
2023-09-14 19:15:23 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -19,12 +19,6 @@ app.use(cors())
 app.use(helmet({
     contentSecurityPolicy: false
 }))
-<<<<<<< HEAD
-
-=======
-app.use(morgan("dev"))
->>>>>>> d872b629af5149d5deccfa2392ae24f333da3801
-
 
 // Lo tuve que hacer asi porque no me tomaba las carpetas en el directotio public
 app.use(express.static(path.join(__dirname + "/src/public"))) 
